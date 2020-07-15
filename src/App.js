0c76e0757e26e6d8562d5d865329691264ba4b1f
raiguard <caleb.heuer@gmail.com>
2020-07-15 11:15:33 -0600

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Hooks from './Components/Hooks';
-import ThemeChanger from './Components/ThemeChanger';
-import RenderProp from './Components/RenderProp';
-import './App.css';
+import React from "react";
+import Hooks from "./Components/Hooks";
+import ThemeChanger from "./Components/ThemeChanger";
+import RenderProp from "./Components/RenderProp";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <Hooks /> */}
+      <Hooks />
       {/* <ThemeChanger /> */}
       {/* <RenderProp /> */}
     </div>
--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-export default props => {
-    return (
-        <div></div>
-    )
-}
+export default (props) => {
+  let [username, setUsername] = useState(""),
+    [count, setCount] = useState(0);
+
+  // empty dependency array simulates a componentDidMount()
+  useEffect(() => {
+    setUsername("Rai");
+  }, []);
+
+  // items in the dependency array will be "watched" and useEffect() will fire whenever those change
+  // unlike componentDidMount, this also fires the first time a component is loaded. watch out for that!
+  useEffect(() => {
+    console.log("Count updated");
+  }, [count]);
+
+  return (
+    <div>
+      <h1>
+        Hello, {username}, your count is {count}
+      </h1>
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      <button onClick={() => setCount(count - 1)}>Count Down</button>
+      <button onClick={() => setCount(count + 1)}>Count Up</button>
+    </div>
+  );
+};
